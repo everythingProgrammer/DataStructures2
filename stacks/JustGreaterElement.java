a0a7d832c82f00f621b5124a8011a146607dc746
Abhinav Rana <ranaabhinav50@gmail.com>
2021-05-03 22:20:39 +0530

    JustGreaterElement Stacks
    using stacks

--- stacks/JustGreaterElement.java
+++ stacks/JustGreaterElement.java
@@ -2,8 +2,6 @@ package stacks;
 import java.util.Stack;
 
 public class JustGreaterElement {
-	
-	
 	public static void nextGreater(int arr[]) {
 		Stack<Integer> s = new Stack<>();
 		s.push(arr[0]);
@@ -24,13 +22,15 @@ public class JustGreaterElement {
 			}
 			s.push(val);
 		}
+		while(!s.isEmpty()) {
+			System.out.println(s.pop());
+		}
 	}
-	
-	
-	
 	public static void main(String args[]) {
 		
-		
+		int arr[] = {50,30,20,40,10,45,5,60,15,8};
+//		int arr[] = {1,2,3,4,3,1,2,3,4,3};
+		nextGreater(arr);
 		
 		
 	}
