52e5f776a578de73b6299554efa194947b1bc6bf
Abhinav Rana <ranaabhinav50@gmail.com>
2021-05-08 05:34:25 +0530

    .

---
+++ stacks/MinStack.java
@@ -0,0 +1,28 @@
+package stacks;
+
+public class MinStack {
+	/* https://leetcode.com/problems/min-stack/submissions/
+	 * 
+	 * 
+	 * MinStack- obtain peek of minstack
+	 * Time O(1)
+	 * Space O(n)
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	
+	
+	/* O(1) time and space complexity */
+	
+	/*use just 1 stack , push all the values greater than min as it is , if any value less than min
+	 * appears then put an encrypted value...*/
+	
+	
+	
+	
+	
+}
